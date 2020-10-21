0ac835d39ee15024fff794aebc5479b000867fa4
Shiyas <shiyas009@gmail.com>
2020-10-21 19:45:25 +0530

    first

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.get('/',(req,res) =>{
+    res.redirect('/newroom')
+})
+
+
+app.get('/:room',(req,res) =>{
+    res.render('room',{roomId:req.params.room})
+})
+
+io.on('connection',socket =>{
+    socket.on('join-room',(roomId,userId) =>{
+        socket.join(roomId)
+        socket.to(roomId).broadcast.emit('user-connected',userId)
+
+        socket.on('disconnect',() =>{
+            socket.to(roomId).broadcast.emit('user-disconnected',userId)
+        })
+    })
+
+    
+})
+const port=process.env.port||300
+server.listen(port,()=>{
+console.log("started")
+}) 
